58dcede1ce6d27d01fb07e0219361db4535a1b5a
matveykylak <matveykylak@gmail.com>
2024-07-26 15:45:55 +0300

    Add project files.

---
+++ HW31DataBaseTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace HW31DataBaseTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
